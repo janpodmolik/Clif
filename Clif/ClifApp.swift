2a9eaee7c42138296756d3afb263dcaaf675ff7f
Jan Podmolik <jan.podmolik@notino.com>
2025-12-07 09:14:19 +0100

    Initial Commit

---
+++ Clif/ClifApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClifApp.swift
+//  Clif
+//
+//  Created by Jan Podmolík on 07.12.2025.
+//
+
+import SwiftUI
+
+@main
+struct ClifApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
